eaef3333bc8763ea3e94a74ecf8e7f7d21b70cda
dnewell <dnewell@netchexonline.com>
2015-03-12 07:51:11 -0500

    Add login function to angular auth service

--- AngularApp/app/services/authService.js
+++ AngularApp/app/services/authService.js
@@ -29,6 +29,30 @@
 			});
 		};
 
+		var _login = function (loginData) {
+
+		    var data = "grant_type=password&username=" + loginData.userName + "&password=" + loginData.password;
+
+		    var deferred = $q.defer();
+
+		    $http.post('/token', data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).success(function (response) {
+
+		        localStorageService.set('authorizationData', { token: response.access_token, userName: loginData.userName });
+
+		        _authentication.isAuth = true;
+		        _authentication.userName = loginData.userName;
+
+		        deferred.resolve(response);
+
+		    }).error(function (err, status) {
+		        _logOut();
+		        deferred.reject(err);
+		    });
+
+		    return deferred.promise;
+
+		};
+
 		var _logOut = function() {
 			localStorageService.remove('authorizationData');
 
